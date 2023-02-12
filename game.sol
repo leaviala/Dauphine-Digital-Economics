e004c76454ccfe8f641348652cf82c07c6522ccb
leaviala <123200014+leaviala@users.noreply.github.com>
2023-02-12 21:45:35 +0100

    Create game.sol

---
+++ game.sol
@@ -0,0 +1,28 @@
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7; 
+
+import "./engine.sol";
+contract game is engine{
+
+    uint randNonce = 0;
+    uint result;
+    function random(uint _modulus) internal returns(uint){
+    randNonce++; 
+    return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender,randNonce))) % _modulus;
+    }
+
+    function gameResults(uint userInput) public returns(uint input1, uint input2, bool output){
+    input1 = random(100);
+    input2 = random(100);
+    result = operator(input1, input2);
+    if (result == userInput){
+                output = true;
+            } else {
+                output = false;
+            }  
+    }
+
+    
+}
